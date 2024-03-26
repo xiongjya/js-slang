5405de6ad165935c41ad578c9160607a3e2afccc
Xiong Jingya <xiongjingyaa@gmail.com>
2024-03-27 01:31:53 +0800

    add go call go-slang (not tested)

--- src/parser/go/index.ts
+++ src/parser/go/index.ts
@@ -6,43 +6,43 @@ import { AcornOptions, Parser } from '../types'
 import { positionToSourceLocation } from '../utils'
 
 export class GoParser implements Parser<AcornOptions> {
-    private chapter: Chapter
-    private variant: Variant
+  private chapter: Chapter
+  private variant: Variant
 
-    constructor(chapter: Chapter, variant: Variant) {
-        this.chapter = chapter
-        this.variant = variant
-    }
+  constructor(chapter: Chapter, variant: Variant) {
+    this.chapter = chapter
+    this.variant = variant
+  }
 
-    parse(
-        programStr: string,
-        context: Context,
-        options?: Partial<AcornOptions>,
-        throwOnError?: boolean
-    ): Program | null {
-        try {
-            // parse the Go code
-            /*
+  parse(
+    programStr: string,
+    context: Context,
+    options?: Partial<AcornOptions>,
+    throwOnError?: boolean
+  ): Program | null {
+    try {
+      // parse the Go code
+      /*
             const chapterNum = 1;
             return parseGoToEstreeAst(programStr, chapterNum, false);
             */
-            return null;
-        } catch (error) {
-            if (error instanceof SyntaxError) {
-                error = new FatalSyntaxError(positionToSourceLocation((error as any).loc), error.toString())
-            }
+      return null
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        error = new FatalSyntaxError(positionToSourceLocation((error as any).loc), error.toString())
+      }
 
-            if (throwOnError) throw error
-            context.errors.push(error)
-        }
-        return null
+      if (throwOnError) throw error
+      context.errors.push(error)
     }
+    return null
+  }
 
-    validate(_ast: Program, _context: Context, _throwOnError: boolean): boolean {
-        return true
-    }
+  validate(_ast: Program, _context: Context, _throwOnError: boolean): boolean {
+    return true
+  }
 
-    toString(): string {
-        return `GoParser{chapter: ${this.chapter}, variant: ${this.variant}}`
-    }
+  toString(): string {
+    return `GoParser{chapter: ${this.chapter}, variant: ${this.variant}}`
+  }
 }
