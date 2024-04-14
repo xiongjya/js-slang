3f4278ea15f44036d53bdcbb3c1cadbfca8abae3
Luo Siyi <lluosiyi@gmail.com>
2024-04-14 15:57:32 +0800

    Use HW compiler and runner instead

--- src/parser/test.ts
+++ src/parser/test.ts
@@ -1,4 +1,3 @@
-
 import createContext from '../createContext'
 import { goRunner } from '../runner/goRunner'
 import { Chapter, Variant } from '../types'
@@ -8,11 +7,11 @@ const program = `
     2+3;
 `
 function main() {
-    const variant = Variant.CONCURRENT
-    const context = createContext(Chapter.GO_1, variant, undefined, undefined)
+  const variant = Variant.CONCURRENT
+  const context = createContext(Chapter.GO_1, variant, undefined, undefined)
 
-    const parsed = parse(program, context)
-    goRunner(parsed)
+  const parsed = parse(program, context)
+  goRunner(parsed)
 }
 
 main()
