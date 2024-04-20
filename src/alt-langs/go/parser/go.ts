3fe2aefe23646eb1194e3dd70180f23dc5805487
Xiong Jingya <66959387+xiongjya@users.noreply.github.com>
2024-04-20 17:32:13 +0800

    [wip] compile pegjs to ts (#27)
    * Compile pegjs to ts file
    
    * Remove unused statements
    
    * Recompile go.pegjs
    
    * Remove unneeded changes

--- src/alt-langs/go/parser/go.ts
+++ src/alt-langs/go/parser/go.ts
@@ -1,34 +1,6 @@
-import * as peggy from "peggy";
-import { LocationRange, Stage } from "peggy";
-import { readFileSync } from "fs";
+import { parse, PeggySyntaxError } from './go_parser';
 
-/**
- * Callback that the gnerated peggy parser uses to show warnings to user
- * @param parserStage
- * @param message
- * @param location
- */
-function warningCallback(
-  parserStage: Stage,
-  message: string,
-  location: LocationRange | undefined,
-): void {
-  console.log(message);
-  //TODO: add location info nicely in future
-}
-
-const parserFilePath = "src/alt-langs/go/parser/go.pegjs"; //this path is relative to where ur terminal is
-const grammar: string = readFileSync(parserFilePath, "utf8");
-
-const parser = peggy.generate(
-  grammar, 
-  {
-  cache: true,
-  warning: warningCallback,
-});
-
-export default function parse(
-  sourceCode: string
-) {
-  return parser.parse(sourceCode);
+export default function go_parse(sourceCode: string) {
+  const parsed_program = parse(sourceCode);
+  return parsed_program;
 }
